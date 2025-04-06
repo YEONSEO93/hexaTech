e030d9333b4a9570b98ca1c94477413025c22bbb
Sorita Heng <69398037+soritaheng@users.noreply.github.com>
2025-04-06 13:28:29 +1000

    Install shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-amber-800 font-bold">Hello HexaTech!</div>;
+  return (
+    <div>
+      <h1 className="text-amber-800 font-bold">Hello HexaTech!</h1>
+      <Button>Start</Button>
+    </div>
+  );
 }
