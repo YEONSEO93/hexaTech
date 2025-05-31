c1f3f7b0592862666d39f4ef9102932ee9e4d2a5
Sorita Heng <69398037+soritaheng@users.noreply.github.com>
2025-05-31 16:27:45 +1000

    remove timeout from feedback message component

--- components/ui/feedback-message.tsx
+++ components/ui/feedback-message.tsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from "react";
-
 interface FeedbackMessageProps {
     type: 'success' | 'error' | 'info' | 'warn';
     message: string;
-    timeout?: number; // in milliseconds, optional
 }
 
-const FeedbackMessage = ({ type, message, timeout = 3000 }: FeedbackMessageProps) => {
-    const [showFeedback, setShowFeedback] = useState(true);
-
-    useEffect(() => {
-        if (!showFeedback) return;
-        const timer = setTimeout(() => setShowFeedback(false), timeout);
-        return () => clearTimeout(timer);
-    }, [showFeedback, timeout]);
-
+const FeedbackMessage = ({ type, message }: FeedbackMessageProps) => {
     const setTextColor = (type: string) => {
         switch (type) {
             case 'success':
@@ -29,8 +18,6 @@ const FeedbackMessage = ({ type, message, timeout = 3000 }: FeedbackMessageProps
                 return '';
         }
     };
-
-    if (!showFeedback) return null;
 
     return (
         <p className={`${setTextColor(type)} text-sm mb-4`}>{message}</p>
